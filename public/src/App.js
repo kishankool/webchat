eb4eb5e221599524b7375e576c42229662d976a9
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-07-28 15:29:09 +0530

    removed some unwanted files

--- public/src/App.js
+++ public/src/App.js
@@ -1,17 +1,10 @@
-import React ,{useEffect, useState}from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 export default function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("https://chatbackend-e3ia.onrender.com")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  },[]);
 
   return (
     <BrowserRouter>

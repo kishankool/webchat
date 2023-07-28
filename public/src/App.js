056d3c738341d6766a3a434c13a551b25c67ca9e
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-07-28 13:01:22 +0530

    hosting changes

--- public/src/App.js
+++ public/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React ,{useEffect, useState}from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://webchat-app-vzsd.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[]);
+
   return (
     <BrowserRouter>
       <Routes>

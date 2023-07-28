85cdc6d57dd84d5bd70163704f5d25619e98c7b5
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-07-28 16:04:46 +0530

    original state

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,26 +18,25 @@ module.exports.login = async (req, res, next) => {
 };
 
 module.exports.register = async (req, res, next) => {
-  return res.json(user1);
-  // try {
-  //   const { username, email, password } = req.body;
-  //   const usernameCheck = await User.findOne({ username });
-  //   if (usernameCheck)
-  //     return res.json({ msg: "Username already used", status: false });
-  //   const emailCheck = await User.findOne({ email });
-  //   if (emailCheck)
-  //     return res.json({ msg: "Email already used", status: false });
-  //   const hashedPassword = await bcrypt.hash(password, 10);
-  //   const user = await User.create({
-  //     email,
-  //     username,
-  //     password: hashedPassword,
-  //   });
-  //   delete user.password;
-  //   return res.json({ status: true, user });
-  // } catch (ex) {
-  //   next(ex);
-  // }
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
 };
 const user1={
   email:'kishanmishra389@gmail.com',
